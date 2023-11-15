7800876e5abacf98edac5fbe612e698a737b4898
elbouhali-nizar <106854283+elbouhali-nizar@users.noreply.github.com>
2023-11-15 14:04:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ fetch('/jsons/education-and-experience.json')
         <div class="education-content">
           <div class="content">
             <div class="year"><i class='bx bxs-calendar'></i> ${item.year}</div>
-            <h3> ${item.institution || item.degree || item.position}</h3>
-                <p>${item.description}</p>
+            <h3> ${item.degree || item.position}</h3>
+            <p>${item.description}</p>
           </div>
         </div>
       `).join('');
